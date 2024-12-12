f638297b1fc38f0cbada882e2d45c2caf1f014c8
VikryAchmadSonjayaKusumah <vikryachmadsoenjayakusumah@gmail.com>
2024-12-12 14:10:40 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,9 +5,17 @@ public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
+        /for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
